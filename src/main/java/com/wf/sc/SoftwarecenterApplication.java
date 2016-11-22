4a476681ab7f3e25d84a79fb23d3d64132c042f7
sanathmadhav <unnimadhav@gmail.com>
2016-11-22 13:54:31 +0530

    Project code base
    Initial code checkin

---
+++ src/main/java/com/wf/sc/SoftwarecenterApplication.java
@@ -0,0 +1,12 @@
+package com.wf.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarecenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwarecenterApplication.class, args);
+	}
+}
